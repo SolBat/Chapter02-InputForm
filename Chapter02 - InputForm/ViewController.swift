bc603e2a788f68f633a4188ae90692149ba629b7
juyoungjung <jk3a0123@gmail.com>
2017-10-22 22:30:14 +0900

    입력필드 커밋

--- Chapter02 - InputForm/ViewController.swift
+++ Chapter02 - InputForm/ViewController.swift
@@ -10,15 +10,131 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var paramEmail: UITextField!
+    
+    var paramUpdate: UISwitch!
+    
+    var paramInterval: UIStepper!
+    
+    var txtUpdate: UILabel!
+    
+    var txtInterval: UILabel!
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.navigationItem.title = "설정"
+        
+        let lblEmail = UILabel()
+        lblEmail.frame = CGRect(x: 30, y: 100, width: 100, height: 30)
+        lblEmail.text = "이메일"
+        lblEmail.font = UIFont.systemFont(ofSize: 14)
+        
+        self.view.addSubview(lblEmail)
+        
+        let lblUpdate = UILabel()
+        lblUpdate.frame = CGRect(x: 30, y: 150, width: 100, height: 30)
+        lblUpdate.text = "자동갱신"
+        lblUpdate.font = UIFont.systemFont(ofSize: 14)
+        
+        self.view.addSubview(lblUpdate)
+        
+        let lblInterval = UILabel()
+        lblInterval.frame = CGRect(x: 30, y: 200, width: 100, height: 30)
+        lblInterval.text = "갱신주기"
+        lblInterval.font = UIFont.systemFont(ofSize: 14)
+        
+        self.view.addSubview(lblInterval)
+        
+        // 입력필드
+        self.paramEmail = UITextField()
+        self.paramEmail.frame = CGRect(x: 120, y: 100, width: 220, height: 30)
+        self.paramEmail.font = UIFont.systemFont(ofSize: 13)
+        self.paramEmail.borderStyle = UITextBorderStyle.roundedRect
+        self.paramEmail.autocapitalizationType = .none
+        
+        self.view.addSubview(self.paramEmail)
+        
+        // 스위치
+        self.paramUpdate = UISwitch()
+        self.paramUpdate.frame = CGRect(x: 120, y: 150, width: 50, height: 30)
+        self.paramUpdate.setOn(true, animated: true)
+        
+        self.view.addSubview(self.paramUpdate)
+        
+        
+        // interval
+        self.paramInterval = UIStepper()
+        self.paramInterval.frame = CGRect(x: 120, y: 200, width: 50, height: 30)
+        self.paramInterval.minimumValue = 0 // 최소값
+        self.paramInterval.maximumValue = 100 // 최대값
+        self.paramInterval.stepValue = 1 // 변경단위
+        self.paramInterval.value = 0 // 초기값
+        
+        self.view.addSubview(self.paramInterval)
+        
+        
+        self.txtUpdate = UILabel()
+        self.txtUpdate.frame = CGRect(x: 250, y: 150, width: 100, height: 30)
+        self.txtUpdate.font = UIFont.systemFont(ofSize: 12)
+        self.txtUpdate.textColor = UIColor.red
+        self.txtUpdate.text = "갱신함"
+        
+        self.view.addSubview(self.txtUpdate)
+        
+        
+        self.txtInterval = UILabel()
+        self.txtInterval.frame = CGRect(x: 250, y: 200, width: 100, height: 30)
+        self.txtInterval.font = UIFont.systemFont(ofSize: 12)
+        self.txtInterval.textColor = UIColor.red
+        self.txtInterval.text = "0분마다"
+        
+        self.view.addSubview(self.txtInterval)
+        
+        
+        self.paramUpdate.addTarget(self, action: #selector(presentUpdateValue(_:)), for: .valueChanged)
+        self.paramInterval.addTarget(self, action: #selector(presentIntervalValue(_:)), for: .valueChanged)
+        
+        let submitBtn = UIBarButtonItem(barButtonSystemItem: .compose, target: self, action: #selector(submit(_:)))
+        
+        self.navigationItem.rightBarButtonItem = submitBtn
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    @objc func presentUpdateValue(_ sender: UISwitch) {
+        self.txtUpdate.text = (sender.isOn == true ? "갱신함" : "갱신하지 않음")
+    }
+    
+    @objc func presentIntervalValue(_ sender: UIStepper) {
+        self.txtInterval.text = ("\( Int(sender.value) )분마다")
+    }
+    
+    @objc func submit(_ sender: Any) {
+        let rvc = ReadViewController()
+        rvc.pEmail = self.paramEmail.text
+        rvc.pUpdate = self.paramUpdate.isOn
+        rvc.pInterval = self.paramInterval.value
+        
+        self.navigationController?.pushViewController(rvc, animated: true)
+    }
+    
+    
+    
+    
+    
+    
+    
+    
 
 
 }
